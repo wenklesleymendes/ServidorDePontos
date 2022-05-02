7c87c9836e75a60ebd99d8777e320cbb8079f772
Wenklesley Mendes <wenklesley.mendes@escolarmanager.com.br>
2022-05-02 06:52:14 -0300

    Cria os Serviços

--- ServidorDePontos.Aplication/Service/GameResultService.cs
+++ ServidorDePontos.Aplication/Service/GameResultService.cs
@@ -1,19 +1,75 @@
-﻿using ServidorDePontos.Aplication.Interface;
+﻿using Microsoft.Extensions.Caching.Memory;
+using ServidorDePontos.Aplication.Interface;
 using ServidorDePontos.Core.Model;
+using ServidorDePontos.Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServidorDePontos.Service.Dto;
 
 namespace ServidorDePontos.Aplication.Service
 {
+    
     public class GameResultService : IGameResultService
     {
-        static List<GameResult> _gameResult;
-        public void PercisteDadosNaMemoria(GameResult gameResult)
+
+        private readonly IMemoryCache memoryCache;
+        private readonly IGameResultRepository _gameResultRepository;
+
+        private readonly string _gameResultKey = "GameResult";
+
+        private static List<GameResult> gameResultList = new List<GameResult>();
+
+        public GameResultService(IMemoryCache memoryCache, IGameResultRepository gameResultRepository)
         {
-            _gameResult.Add(gameResult);
+            this.memoryCache = memoryCache;
+            _gameResultRepository = gameResultRepository;
+        }
+
+        public void PersisteDadosNaMemoria(List<GameResult> gameResults)
+        {
+            gameResultList.AddRange(from result in gameResults
+                                    select result);
+
+            memoryCache.Set(_gameResultKey, gameResultList);          
+
+            memoryCache.TryGetValue(_gameResultKey, out List<GameResult> value);
+
+            if(value.Count == 20)
+            {
+                PersisteDadosNoBanco();
+            }
+        }
+
+        public void PersisteDadosNoBanco()
+        {
+            memoryCache.TryGetValue(_gameResultKey, out List<GameResult> value);
+            _gameResultRepository.AddRangeAsync(value);
+
+            gameResultList.Clear();
+        }
+
+        public List<LeaderBoardDto> ConsultaDadosPersistidos()
+        {
+            List<LeaderBoardDto> leaderBoards = new();
+
+            var result = _gameResultRepository.Busca100Primeiros();
+
+            foreach (var teste in result)
+            {
+                LeaderBoardDto leaderBoard = new()
+                {
+                    PlayerId = teste.PlayerId,
+                    Balance = teste.Win,
+                    LastUpdateDate = teste.Timestamp,
+                };
+
+                leaderBoards.Add(leaderBoard);
+            }
+
+            return leaderBoards;
         }
     }
 }
