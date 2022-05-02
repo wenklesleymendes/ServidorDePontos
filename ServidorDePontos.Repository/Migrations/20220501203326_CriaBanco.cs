136e7d956c7bd323b1733bcf8a7e281a54fd4f2e
Wenklesley Mendes <wenklesley.mendes@escolarmanager.com.br>
2022-05-02 06:50:47 -0300

    Cria as Migrations

---
+++ ServidorDePontos.Repository/Migrations/20220501203326_CriaBanco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServidorDePontos.Repository.Migrations
+{
+    public partial class CriaBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameResult",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PlayerId = table.Column<long>(type: "INTEGER", nullable: false),
+                    GameId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Win = table.Column<long>(type: "INTEGER", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameResult", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameResult");
+        }
+    }
+}
