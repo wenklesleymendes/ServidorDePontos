f1193af3b73eca5c61bab32b951c37b76db1f2d2
Wenklesley Mendes <wenklesley.mendes@escolarmanager.com.br>
2022-05-02 06:44:34 -0300

    Adiciona as Controllers

---
+++ ServidorDePontos.API/Controllers/GameResultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServidorDePontos.Aplication.Interface;
+using ServidorDePontos.Core.Model;
+
+namespace ServidorDePontos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameResultController : Controller
+    {
+        private readonly IGameResultService gameResultService;
+
+        public GameResultController(IGameResultService gameResultService)
+        {
+            this.gameResultService = gameResultService;
+        }
+
+        [Route("EnviadadosAoServidor")]
+        [HttpPost]
+        public IActionResult PersistiDados(List<GameResult> dados)
+        {
+            gameResultService.PersisteDadosNaMemoria(dados);
+
+            return Ok();
+        }
+    }
+}
---
+++ ServidorDePontos.API/Controllers/LeaderBoardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServidorDePontos.Aplication.Interface;
+
+namespace ServidorDePontos.API.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaderBoardController : Controller
+    {
+        private readonly IGameResultService gameResultService;
+
+        public LeaderBoardController(IGameResultService gameResultService)
+        {
+            this.gameResultService = gameResultService;
+        }
+
+        [Route("consulta")]
+        [HttpGet]
+        public IActionResult ContsultaGameResult()
+        {
+            var result = gameResultService.ConsultaDadosPersistidos();
+
+            return Ok(result);
+        }
+    }
+}
