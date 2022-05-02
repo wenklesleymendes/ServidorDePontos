ca8f406dbf3144cf7184f0427dc105bcdb3dd1e3
Wenklesley Mendes <wenklesley.mendes@escolarmanager.com.br>
2022-05-02 06:50:30 -0300

    Inclui Configuração EF

---
+++ ServidorDePontos.Repository/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServidorDePontos.Core.Model;
+
+namespace ServidorDePontos.Repository
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<GameResult> GameResult { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
--- ServidorDePontos.Repository/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServidorDePontos.Repository
-{
-    public class Class1
-    {
-
-    }
-}
